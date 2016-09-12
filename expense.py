767c5f6d6a0e8a1536a790a40f3865fc9b953c1f
Adam Sterner <adam@kratta.se>
2016-09-12 21:20:12 +0200

    Adds an Expense class and an __init__-function

--- expense.py
+++ expense.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+class Expense:
+    """Common class for all expenses"""
+    expense_count = 0
+
+    def __init__(self, name, price):
+        pass
+
